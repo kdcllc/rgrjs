c78f31713a5e3e9e29c7b4a4a1ad8e588b802f46
KDCLLC <info@kingdavidconsulting.com>
2016-01-22 15:24:57 -0500

    Transition to Flux

--- js/components/Main.js
+++ js/components/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import API from '../API';
-import LinkStore from "../stores/LinkStore";
+
 
 let _getAllState = () => {
     return { links: LinkStore.getAll() };
@@ -15,43 +14,7 @@ class Main extends React.Component {
         limit: 3
     };
 
-    state = _getAllState();
-    
-     //removed before added stage-0
-    // constructor(props) {
-    //     super(props);
-    //     //removed before added stage-0
-    //     //this.state = _getAllState();
-    //     this.onChange = this.onChange.bind(this);
-    // }
-    
-    // componentWillMount() {
-    //     debugger;
-    // }
-    componentDidMount() {
-        //debugger;
-        
-        API.fetchLinks();
-        LinkStore.on('change', this.onChange);
-    }
-    componentWillUnmount() {
-        LinkStore.removeListerner('change', this.onChange);
-    };
-
-    onChange = () => {
-        console.log('4. In the View');
-
-        this.setState(_getAllState);
-    };
-    
-     //removed before added stage-0
-    //     onChange() {
-    //         console.log('4. In the View');
-    // 
-    //         this.setState(_getAllState);
-    //     }
-
-    render() {
+   render() {
         let content = this.state.links.slice(0, this.props.limit).map(link => {
             return <li key={ link._id }>
             <a href={ link.url }>{ link.title } < /a>
